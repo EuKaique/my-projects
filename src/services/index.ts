cf38d00f7ff06954b854240c04117d5b51232a65
Kaique <kaiqueols75@gmail.com>
2025-11-24 19:26:51 -0300

    chore: adicionando card do saas dashboard

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,87 +1,94 @@
-import Card01 from '../assets/img/card01.png';
-import Card02 from '../assets/img/card02.png';
-import Card03 from '../assets/img/card03.png';
-import Card04 from '../assets/img/card04.png';
+import Card01 from "../assets/img/card01.png";
+import Card02 from "../assets/img/card02.png";
+import Card03 from "../assets/img/card03.png";
+import Card04 from "../assets/img/card04.png";
+import Card05 from "../assets/img/card05.png";
 
 export const slides = [
   {
     id: 1,
     title: "Webflow para gestão",
     description: "Template e ferramentas para o seu negócio.",
-    image: Card01
+    image: Card01,
   },
   {
     id: 2,
-    title: "Netflix Clone",
-    description: "Protótipo de clone da Netflix.",
-    image: Card02
+    title: "Base - Saas",
+    description: "Mini saas para o seu negócio.",
+    image: Card05,
   },
   {
     id: 3,
+    title: "Netflix Clone",
+    description: "Protótipo de clone da Netflix.",
+    image: Card02,
+  },
+  {
+    id: 4,
     title: "Mercado rápido",
     description: "Ecommerce para o seu negócio.",
-    image: Card03
+    image: Card03,
   },
 ];
 export const cards = [
-    {
-        id: 1,
-        image: Card01,
-        link: "https://consulting-webflow-mdzgmc80z-eukaique.vercel.app"
-    },
-    {
-        id: 2,
-        image: Card02,
-        link: "https://netflix-clone-yd6r-oy5kqogjy-eukaique.vercel.app"
-    },
-    {
-        id: 3,
-        image: Card03,
-        link: "https://eukaique.github.io/Ecommerce"
-    },
-        {
-        id: 4,
-        image: Card04,
-        link: "https://eukaique.github.io/Agencia-servicos"
-    },
-    {
-        id: 5,
-        image: "https://picsum.photos/id/1025/800/400",
-        link: "https://google.com"
-    },
-        {
-        id: 6,
-        image: "https://picsum.photos/id/1035/800/400",
-        link: "https://google.com"
-    },
-    {
-        id: 7,
-        image: "https://picsum.photos/id/1025/800/400",
-        link: "https://google.com"
-    },
-        {
-        id: 1,
-        image: "https://picsum.photos/id/1035/800/400",
-        link: "https://google.com"
-    },
-    {
-        id: 8,
-        image: "https://picsum.photos/id/1025/800/400",
-        link: "https://google.com"
-    },
-        {
-        id: 9,
-        image: "https://picsum.photos/id/1035/800/400",
-        link: "https://google.com"
-    },
-    {
-        id: 10,
-        image: "https://picsum.photos/id/1025/800/400",
-        link: "https://google.com"
-    },
-    {
-        id: 11,
-        image: "https://picsum.photos/id/1025/800/400",
-        link: "https://google.com"
-    },
+  {
+    id: 1,
+    image: Card01,
+    link: "https://consulting-webflow-mdzgmc80z-eukaique.vercel.app",
+  },
+  {
+    id: 2,
+    image: Card02,
+    link: "https://netflix-clone-yd6r-oy5kqogjy-eukaique.vercel.app",
+  },
+  {
+    id: 3,
+    image: Card03,
+    link: "https://eukaique.github.io/Ecommerce",
+  },
+  {
+    id: 4,
+    image: Card04,
+    link: "https://eukaique.github.io/Agencia-servicos",
+  },
+  {
+    id: 5,
+    image: Card05,
+    link: "https://saas-dashboard-flame.vercel.app",
+  },
+  // {
+  //   id: 6,
+  //   image: "https://picsum.photos/id/1035/800/400",
+  //   link: "https://google.com",
+  // },
+  // {
+  //   id: 7,
+  //   image: "https://picsum.photos/id/1025/800/400",
+  //   link: "https://google.com",
+  // },
+  // {
+  //   id: 1,
+  //   image: "https://picsum.photos/id/1035/800/400",
+  //   link: "https://google.com",
+  // },
+  // {
+  //   id: 8,
+  //   image: "https://picsum.photos/id/1025/800/400",
+  //   link: "https://google.com",
+  // },
+  // {
+  //   id: 9,
+  //   image: "https://picsum.photos/id/1035/800/400",
+  //   link: "https://google.com",
+  // },
+  // {
+  //   id: 10,
+  //   image: "https://picsum.photos/id/1025/800/400",
+  //   link: "https://google.com",
+  // },
+  // {
+  //   id: 11,
+  //   image: "https://picsum.photos/id/1025/800/400",
+  //   link: "https://google.com",
+  // },
 ];
